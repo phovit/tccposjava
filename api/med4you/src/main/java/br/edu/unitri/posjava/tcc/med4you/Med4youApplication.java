40e097fcffd9cd472fb102cd74bb439aa3f3ce2a
edufratari <edufratari@hotmail.com>
2018-07-16 20:30:02 -0300

    creating project

---
+++ api/med4you/src/main/java/br/edu/unitri/posjava/tcc/med4you/Med4youApplication.java
@@ -0,0 +1,12 @@
+package br.edu.unitri.posjava.tcc.med4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Med4youApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Med4youApplication.class, args);
+	}
+}
