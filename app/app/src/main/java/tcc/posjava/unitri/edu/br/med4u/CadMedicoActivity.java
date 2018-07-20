de6c8ee3e31213f1d470b48e31f58236ca4a9d47
FilipeSanto <filipe.francisco3@gmail.com>
2018-07-19 23:03:17 -0300

    Primeiro esboço

---
+++ app/app/src/main/java/tcc/posjava/unitri/edu/br/med4u/CadMedicoActivity.java
@@ -0,0 +1,13 @@
+package tcc.posjava.unitri.edu.br.med4u;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadMedicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_medico);
+    }
+}
