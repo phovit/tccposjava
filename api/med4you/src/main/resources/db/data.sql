43f365c6cd0f1d6b0dff54c64bbcaac84f065949
Paulo Henrique <paulo.15.paulo@gmail.com>
2018-07-22 20:52:20 -0300

    inserindo dados

---
+++ api/med4you/src/main/resources/db/data.sql
@@ -0,0 +1,7 @@
+insert into medicine (id, name, generic) values (1, 'Dorflex',0);
+insert into medicine (id, name, generic) values (2, 'Amoxilina',0);
+insert into medicine (id, name, generic) values (3, 'Minoxidil',0);
+insert into medicine (id, name, generic) values (4, 'Dipirona',0);
+insert into medicine (id, name, generic) values (5, 'Bromoprida',0);
+insert into medicine (id, name, generic) values (6, 'Carvedilol',0);
+insert into medicine (id, name, generic) values (7, 'Rivotril',0);
