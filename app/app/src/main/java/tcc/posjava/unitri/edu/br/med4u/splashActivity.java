504d2a4f8e4fde6341d0858f8805679930d87573
FilipeSanto <filipe.francisco3@gmail.com>
2018-07-31 21:30:08 -0300

    Inclusao de Splash

---
+++ app/app/src/main/java/tcc/posjava/unitri/edu/br/med4u/splashActivity.java
@@ -0,0 +1,36 @@
+package tcc.posjava.unitri.edu.br.med4u;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class splashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                chamaPrincipal();
+            }
+        }, 2000);
+
+    }
+    private void chamaPrincipal() {
+        Intent intent = new Intent(splashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
